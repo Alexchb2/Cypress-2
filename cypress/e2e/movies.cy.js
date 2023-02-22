21289779d73f364a27bad5d8d269470410a81c7e
alexc <alexchb.makarenko@yandex.ru>
2023-02-22 19:47:43 +0300

    cypress 2

--- cypress/e2e/movies.cy.js
+++ cypress/e2e/movies.cy.js
@@ -1,6 +1,3 @@
-const sets = require('../fixtures/seats.json')
-const user = require('../fixtures/user.json')
-const text = require('../fixtures/text.json')
 
 describe('movie selection screen', () => {
 
@@ -10,35 +7,10 @@ describe('movie selection screen', () => {
     cy.get('.page-nav__day').should('have.length', 7)
   })
 
-  sets.forEach((current) => {
-    it('reserves tickets', () => {
-      cy.visit('/')
-  
-      cy.get('.page-nav__day').eq(3).click()
-      cy.get('.movie').contains('21:00').click()
-        current.data.forEach((seat) => {
-          cy.get(`.buying-scheme__wrapper > :nth-child(${seat.row}) > :nth-child(${seat.seat})`).click()
-        })
-    })
+  it('shows schedule for 3 movie', () => {
+    cy.visit('/')
+
+    cy.get('body > main > section').should('have.length', 3)
   })
-  
-  it('Buy tickets', () => {
-    cy.visit('/admin/')
-    cy.login(user.email, user.password)
-    cy.get(text.testHall).then(($el) => $el.textContent).should('have.text','Логан');
-    cy.get(text.testHall).invoke('text').then((text) => {
-      cy.visit("qamid.tmweb.ru");
-      cy.get('.page-nav__day').eq(3).click() 
-      cy.get(text.testHall).should('have.text', text);
-    })
-    sets.forEach((current) => {
-        cy.visit('/')
-    
-        cy.get('.page-nav__day').eq(3).click()
-        cy.get('.movie').contains('21:00').click()
-          current.data.forEach((seat) => {
-            cy.get(`.buying-scheme__wrapper > :nth-child(${seat.row}) > :nth-child(${seat.seat})`).click()
-          })
-      })
-  })
+
 })
